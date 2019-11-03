767dfe4833c0f452299e5d1a092eb67ea3ad49b5
cesarazocar <cesar.azocar.pontigo@gmail.com>
2019-11-03 00:10:15 -0300

    First Commit
    Angular highchart

---
+++ src/app/components/output-graph/output-graph.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+
+//------------------Added-------------
+import * as Highcharts from 'highcharts';
+import { HttpClient } from '@angular/common/http';
+import { interval, Subscription } from 'rxjs';
+
+declare var require: any;
+let Boost = require('highcharts/modules/boost');
+let noData = require('highcharts/modules/no-data-to-display');
+let More = require('highcharts/highcharts-more');
+
+Boost(Highcharts);
+noData(Highcharts);
+More(Highcharts);
+noData(Highcharts);
+//------------------Added-------------
+
+@Component({
+  selector: 'app-output-graph',
+  templateUrl: './output-graph.component.html',
+  styleUrls: ['./output-graph.component.css']
+})
+export class OutputGraphComponent implements OnInit {
+
+  data:any[];
+//------------------Added-------------
+public options: any = {
+  chart: {
+    type: 'scatter',
+    height: 700
+  },
+  title: {
+    text: 'Sample Scatter Plot'
+  },
+  credits: {
+    enabled: false
+  },
+  tooltip: {
+    formatter: function() {
+      return 'x: ' + Highcharts.dateFormat('%e %b %y %H:%M:%S', this.x) +' y: ' + this.y.toFixed(2);
+    }
+  },
+  xAxis: {
+    type: 'datetime',
+    labels: {
+      formatter: function() {
+        return Highcharts.dateFormat('%e %b %y', this.value);
+      }
+    }
+  },
+  series: [
+    {
+      name: 'Normal',
+      turboThreshold: 500000,
+      data: []
+    },
+    {
+      name: 'Abnormal',
+      turboThreshold: 500000,
+      data: []
+    }
+  ]
+}
+subscription: Subscription;
+//------------------Added-------------
+  
+
+  constructor(private http: HttpClient) { 
+    
+  } //httpclient added
+
+  ngOnInit() {
+
+
+
+
+
+
+
+    //---added
+     // Set 10 seconds interval to update data again and again
+     const source = interval(10000);
+    
+     // Sample API
+     const apiLink = 'https://api.myjson.com/bins/13lnf4';
+ 
+     this.subscription = source.subscribe(val => this.getApiResponse(apiLink).then(
+       (data:any[]) => { //definir que es un arreglo de forma explicita
+         const updated_normal_data = [];
+         const updated_abnormal_data = [];
+        
+
+         data.forEach(row => {
+           const temp_row = [
+             new Date(row.timestamp).getTime(),
+             row.value
+           ];
+           row.Normal === 1 ? updated_normal_data.push(temp_row) : updated_abnormal_data.push(temp_row);
+         });
+         this.options.series[0]['data'] = updated_normal_data;
+         this.options.series[1]['data'] = updated_abnormal_data;
+         Highcharts.chart('container', this.options);
+       },
+       error => {
+         console.log('Something went wrong.');
+       })
+     );
+     //----added
+
+
+
+
+     
+  }
+  getApiResponse(url) {
+    return this.http.get(url, {})
+      .toPromise().then(res => {
+        return res;
+      });
+  }
+}
---
+++ src/app/models/DataI.ts
@@ -0,0 +1,9 @@
+export interface DataI {
+
+    id?: number; //? = opcional
+    title?: string;
+    description?: string;
+    image?: string;
+    created_at?: Date;
+
+}
